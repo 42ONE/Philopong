5cd4d6020a7c68198642111384cda992304c7897
sed-int <nouveau0309@gmail.com>
2024-05-29 17:06:37 +0900

    revert: models, views, test 모두 원래로 되돌림

--- backend/members/models.py
+++ backend/members/models.py
@@ -1,15 +1,2 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
-class User(AbstractUser):
-    email = models.EmailField(unique=True)
-    social_access_token = models.CharField(max_length=255, null=True, blank=True)
-    social_refresh_token = models.CharField(max_length=255, null=True, blank=True)
-    token_expiration_time = models.DateTimeField(null=True, blank=True)
-    avatar = models.CharField(max_length=255, null=True, blank=True)
-    lang = models.CharField(max_length=10, default='en')
-    created_at = models.DateTimeField(auto_now_add=True)
-
-class Friend(models.Model):
-    my_id = models.ForeignKey(User, related_name='my_friends', on_delete=models.CASCADE)
-    friend_id = models.ForeignKey(User, related_name='friends_with_me', on_delete=models.CASCADE)
