f485ff9a3c68f9d8548800e4ad96d2aaa7a63ff4
wooseong <wooseong1106@gmail.com>
2024-06-05 18:03:36 +0900

    refact: profile 페이지 info, match history 데이터 변수로 바꿀 수 있도록 개선함

--- frontend/js/Profile.js
+++ frontend/js/Profile.js
@@ -1,9 +1,31 @@
 import Component from "./core/Component.js";
-import Avatar from "./components/Avatar.js";
+import Title from "./components/Title.js";
 import Info from "./components/Info.js";
 import MatchHistory from "./components/MatchHistory.js";
 
 export default class Profile extends Component {
+	setup () {
+		this.state = {
+			user_data : {
+                info: [
+                    {
+                        profile: "./images/philopong.webp",
+                        id: "test_id",
+                        nickname: "test_nickname",
+                        win: 10,
+                        lose: 5
+                    }
+                ],
+				history_list: [
+					{ opponent_id: "woosekim", result: "win", o_score: 3, u_score: 11 },
+					{ opponent_id: "phan", result: "lose", o_score: 2, u_score: 0 },
+					{ opponent_id: "hcho2", result: "win", o_score: 3, u_score: 11 },
+					{ opponent_id: "soohlee", result: "lose", o_score: 8, u_score: 5 },
+					{ opponent_id: "soohlee", result: "lose", o_score: 8, u_score: 5 }
+				]
+			}
+		}
+	}
 
     template() {
         return `
@@ -38,12 +60,9 @@ export default class Profile extends Component {
                 </div>
                     
                 <div class="row">
-                    <h1 class="py-3 title">Profile</h1>
+                    <div class="text-point-color d-flex justify-content-center py-3 title" data-component="title"></div>
                 </div>
-                <div class="row">   
-                    <div class="col-md" data-component="avatar"></div>
-                    <div class="col-md info" data-component="info"></div>
-                </div>
+                <div class="row" data-component="info"></div>
                 <div class="profile-edit">
                     <span class="btn">Profile Edit</span>
                 </div>
@@ -55,14 +74,22 @@ export default class Profile extends Component {
         `;
     }
     
-    mounted()
-    {
-        const $Avatar = this.$target.querySelector('[data-component="avatar"]');
+    mounted() {
+        const { getPageTitle } = this;
+
+		const $title = this.$target.querySelector('[data-component="title"]');
         const $Info = this.$target.querySelector('[data-component="info"]');
         const $MatchHistory = this.$target.querySelector('[data-component="match-history"]');
 
-        new Avatar($Avatar);
-        new Info($Info);
-        new MatchHistory($MatchHistory);
+        new Title($title, { getPageTitle });
+        
+        const { info, history_list } = this.state.user_data;
+
+        new Info($Info, { info: info[0] });
+        new MatchHistory($MatchHistory, { match_history: history_list, user_id: info[0].id });
+    }
+
+    get getPageTitle() {
+        return 'Profile';
     }
 }
