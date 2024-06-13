f3ebe9270c95e06e1364ab7d83922f83df354bd3
Hyun-Soon <s7514036@naver.com>
2024-06-13 14:13:21 +0900

    fix : mainpage

--- frontend/js/MainPage.js
+++ frontend/js/MainPage.js
@@ -1,31 +1,48 @@
 import Component from "./core/Component.js";
 import Title from "./components/Title.js";
-import Sidebar from "./components/Sidebar.js";
 
 export default class MainPage extends Component{
     template() {
       return `
-      <div class="text-point-color d-flex justify-content-center py-3 title" data-component="title"></div>
-      <div class="sidebar" data-component="sidebar"></div>
-      <div class="game-example" data-component="mainSection"></div>
+      <header class="text-point-color d-flex justify-content-center py-3 title" data-component="title"></header>
+      <div class="container centered-container">
+        <div class="text-center d-flex flex-column justify-content-center align-items-center">
+          <button class="btn btn-primary btn-lg mb-2 d-block" id="btn1v1">1 vs 1</button>
+          <button class="btn btn-primary btn-lg mb-2 d-block" id="btnTournament">Tournament</button>
+          <button class="btn btn-primary btn-lg mb-2 d-block" id="btnMultiplay">Multiplay</button>
+          <hr>
+          <hr>
+          <hr>
+          <hr>
+          <hr>
+          <div class="dropdown">
+            <button class="btn btn-primary btn-small mb-2 d-block dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+              Language
+            </button>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+              <li><a class="dropdown-item" href="#">English</a></li>
+              <li><a class="dropdown-item" href="#">Korean</a></li>
+              <li><a class="dropdown-item" href="#">Tagalog</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
       `;
     }
 
     get getPageTitle () {
-		return 'Philopong';
-	}
+      return 'Philopong';
+    }
 
     mounted () {
         const { getPageTitle } = this;
         const $title = this.$target.querySelector('[data-component="title"]');
-        const $sidebar = this.$target.querySelector('[data-component="sidebar"]');
-    
-        new Title($title, {
-          getPageTitle,
-        });
-
-        new Sidebar($sidebar);
+        // const $btn1v1 = this.$target.querySelector('#btn1v1');
+        // const $btnTournament = this.$target.querySelector('#btnTournament');
+        // const $btnMultiplay = this.$target.querySelector('#btnMultiplay');
+        
+            new Title($title, {
+              getPageTitle,
+            });
       }
-
-
   }
