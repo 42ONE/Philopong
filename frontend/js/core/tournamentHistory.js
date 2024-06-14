9dd7b556e51d18a571f6ba9a66685479456b2805
Hyun-Soon <s7514036@naver.com>
2024-06-14 19:31:06 +0900

    feat : tournamentHistory.js

---
+++ frontend/js/core/tournamentHistory.js
@@ -0,0 +1,25 @@
+window.tournament = {
+    tournament: {
+        match1: {
+            user1: '',
+            user2: '',
+            score1: 0,
+            score2: 0,
+            winner: '',
+        },
+        match2: {
+            user1: '',
+            user2: '',
+            score1: 0,
+            score2: 0,
+            winner: '',
+        },
+        match3: {
+            user1: '',
+            user2: '',
+            score1: 0,
+            score2: 0,
+            winner: '',
+        },
+    }
+}
