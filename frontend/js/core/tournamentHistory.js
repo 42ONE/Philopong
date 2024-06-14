cb2e6a433bd4c4b39f3a5950533e05082607551b
Hyun-Soon <s7514036@naver.com>
2024-06-14 19:51:58 +0900

    tournamenthistory

--- frontend/js/core/tournamentHistory.js
+++ frontend/js/core/tournamentHistory.js
@@ -1,25 +1,26 @@
-window.tournament = {
-    tournament: {
-        match1: {
-            user1: '',
-            user2: '',
-            score1: 0,
-            score2: 0,
-            winner: '',
-        },
-        match2: {
-            user1: '',
-            user2: '',
-            score1: 0,
-            score2: 0,
-            winner: '',
-        },
-        match3: {
-            user1: '',
-            user2: '',
-            score1: 0,
-            score2: 0,
-            winner: '',
-        },
-    }
+var tournament = {
+    match1: {
+        user1: '',
+        user2: '',
+        score1: 0,
+        score2: 0,
+        winner: '',
+    },
+    match2: {
+        user1: '',
+        user2: '',
+        score1: 0,
+        score2: 0,
+        winner: '',
+    },
+    match3: {
+        user1: '',
+        user2: '',
+        score1: 0,
+        score2: 0,
+        winner: '',
+    },
 }
+
+localStorage.setItem('tournament', tournament);
+//localStorage.getItem('tournament');로 꺼내다 쓰시면 됩니다
