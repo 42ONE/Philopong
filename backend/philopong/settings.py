647d4ae86e02c01415a351ff410b5c88f12abb70
Sooha Lee <soohlee@c1r9s4.42seoul.kr>
2024-07-02 15:38:30 +0900

    fix : django에 setting.py파일에 시크릿키 env파일로 이동

--- backend/philopong/settings.py
+++ backend/philopong/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-85xf_ra6)eck^4)u-%ji0t9zxdm&5h)c7=goj!zzlj$qe+sy_l'
+SECRET_KEY = os.getenv('ENV_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
